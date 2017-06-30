47ef1f9fd0f91cabd29d4c70910d7adf8f752ab3
Flip Sasser <flip@inthebackforty.com>
2017-06-30 11:08:34 -0400

    More clearly namespace the Oppificator ... but why?

--- lib/oppificator.rb
+++ lib/oppificator.rb
@@ -1,22 +1,24 @@
-class Oppificator
+module Oppify
+  class Oppificator
 
-  attr_reader :input
+    attr_reader :input
 
-  def initialize(input)
-    @input = input
+    def initialize(input)
+      @input = input
+    end
+
+    def opoppopexop
+      '([a-z&&[^aeiouy]])'
+    end
+
+    def oppify
+      input.gsub(/#{opoppopexop}/i, '\1op')
+    end
+
+    def deoppify
+      input.gsub(/#{opoppopexop}op/i, '\1')
+    end
+
   end
-
-  def opoppopexop
-    '([a-z&&[^aeiouy]])'
-  end
-
-  def oppify
-    input.gsub(/#{opoppopexop}/i, '\1op')
-  end
-
-  def deoppify
-    input.gsub(/#{opoppopexop}op/i, '\1')
-  end
-
 end
 
